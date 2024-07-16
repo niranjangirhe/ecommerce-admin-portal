65e2052ec44af21fccc24fda2e3207bd808ce085
niranjangirhe <niranjangirheindia@gmail.com>
2024-07-17 01:41:20 +0530

    fix: documentation scroll

--- app/(dashboard)/docs/(routes)/components/main-content.tsx
+++ app/(dashboard)/docs/(routes)/components/main-content.tsx
@@ -5,10 +5,15 @@ interface MainContentProps {
 }
 
 const MainContent: React.FC<MainContentProps> = ({ sections }) => {
+  const length = sections.length;
   return (
     <div className="space-y-6">
-      {sections.map((section) => (
-        <section key={section.id} id={section.id}>
+      {sections.map((section, index) => (
+        <section
+          key={section.id}
+          id={section.id}
+          className={index === length - 1 ? "min-h-[calc(100vh-48px)]" : ""}
+        >
           <h2 className="text-xl font-bold mb-4">{section.title}</h2>
           {section.content}
           <Separator className="mt-4" />
