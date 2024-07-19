fcf0eb1e51ca33dbd851bf3f38d9da2b3c54978e
niranjangirhe <niranjangirheindia@gmail.com>
2024-07-20 01:03:55 +0530

    fix: attempted to fix webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,9 +1,10 @@
 import Stripe from "stripe";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { stripe } from "@/lib/strip";
 import prismadb from "@/lib/prismadb";
+import getRawBody from "raw-body";
+import { NextApiRequest } from "next";
 
 export const config = {
   api: {
@@ -11,19 +12,19 @@ export const config = {
   }
 };
 
-export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
+export async function POST(req: NextApiRequest) {
+  const rawBody = await getRawBody(req);
+  const signature = req.headers["stripe-signature"] as string;
 
-  console.log("Webhook called");
-  console.log("Raw body:", body);
+  console.log("body:", JSON.stringify(req.body, null, 2));
+  console.log("Raw body:", rawBody);
   console.log("Signature:", signature);
 
   let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(
-      body,
+      rawBody,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
