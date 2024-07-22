dbd628187fa1a1f107a658d00f43e665b2679d6f
niranjangirhe <niranjangirheindia@gmail.com>
2024-07-23 00:00:07 +0530

    fix: CORS

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -7,7 +7,7 @@ import prismadb from "@/lib/prismadb";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function OPTIONS() {
